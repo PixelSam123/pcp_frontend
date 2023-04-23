cc1b2dfb7667c5dc6673f025bca92c78eb7bedb5
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-04-23 21:47:56 +0700

    add working login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,15 +1,37 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:pcp_frontend/components.dart';
+import 'package:pcp_frontend/secure_storage.dart';
+import 'package:pcp_frontend/settings.dart';
 import 'package:pcp_frontend/sizes.dart';
+import 'package:pcp_frontend/types.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
+  void _logout(BuildContext context) {
+    context.read<SecureStorage>().loginToken = null;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const PageLayout(
+    return PageLayout(
       title: 'Login',
-      child: _CredentialsForm(),
+      child: Column(children: [
+        context.select((SecureStorage s) => s.loginToken) == null
+            ? const _CredentialsForm()
+            : Column(children: [
+                const Text('You are logged in!'),
+                const SizedBox(height: PadSize.sm),
+                ElevatedButton(
+                  onPressed: () => _logout(context),
+                  child: const Text('Log out'),
+                ),
+              ]),
+      ]),
     );
   }
 }
@@ -24,6 +46,36 @@ class _CredentialsForm extends StatefulWidget {
 class _CredentialsFormState extends State<_CredentialsForm> {
   final _username = TextEditingController();
   final _password = TextEditingController();
+
+  Future<AccessTokenResponse>? _tokenResponse;
+
+  Future<AccessTokenResponse> _fetchToken() async {
+    final appSettings = context.read<AppSettings>();
+    final response = await http.post(
+      Uri.parse('${appSettings.serverUrl}/token/'),
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      body: {
+        'username': _username.text,
+        'password': _password.text,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return AccessTokenResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to fetch token (log in):\n${response.body}');
+    }
+  }
+
+  Future<void> _login() async {
+    final token = _fetchToken();
+    setState(() {
+      _tokenResponse = token;
+    });
+
+    final secureStorage = context.read<SecureStorage>();
+    secureStorage.loginToken = (await token).accessToken;
+  }
 
   @override
   void dispose() {
@@ -60,9 +112,25 @@ class _CredentialsFormState extends State<_CredentialsForm> {
           ),
           const SizedBox(height: PadSize.md),
           ElevatedButton(
-            onPressed: () {},
-            child: const Text('Submit'),
+            onPressed: _login,
+            child: const Text('Login'),
           ),
+          const SizedBox(height: PadSize.md),
+          _tokenResponse == null
+              ? const Text('Waiting for login...')
+              : FutureBuilder(
+                  future: _tokenResponse,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return Text(snapshot.error.toString());
+                    }
+                    if (!snapshot.hasData) {
+                      return const Text('Logging in...');
+                    }
+
+                    return const Text('You are logged in!');
+                  },
+                ),
         ],
       ),
     );
--- lib/types.dart
+++ lib/types.dart
@@ -1,3 +1,20 @@
+class AccessTokenResponse {
+  const AccessTokenResponse({
+    required this.accessToken,
+    required this.tokenType,
+  });
+
+  final String accessToken;
+  final String tokenType;
+
+  factory AccessTokenResponse.fromJson(Map<String, dynamic> json) {
+    return AccessTokenResponse(
+      accessToken: json['access_token'],
+      tokenType: json['token_type'],
+    );
+  }
+}
+
 abstract class _UserBase {
   const _UserBase({required this.name});
 
